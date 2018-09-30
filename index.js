0f6fba85fd2c2daa6b3d8889839ff6a5de091d5b
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 14:05:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray){
+  var emptyArr = [];
+  for(var i = 0; i < musiciansArray.length; i++){
+    emptyArr.push(musiciansArray[i] + " plays " + instrumentsArray[i])
+  }
+  
+  return emptyArr;
+}
